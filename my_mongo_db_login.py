0088ba164b64de478602e80fad97b82eea0a4a96
Luis Amaral [amaral@northwestern.edu] <amaral@northwestern.edu>
2018-05-01 11:38:20 -0500

    Provide example of mongo login info

--- my_mongo_db_login.py
+++
@@ -1,4 +0,0 @@
-DB_LOGIN_INFO = {'credentials': 'mongodb://ghostwriter:safetynotguaranteed@chicago.chem-eng.northwestern.edu',
-                 'port': 27017}
-
-DATABASE_NAME = 'amaral_cv_data'
---
+++ my_mongo_db_loginEXAMPLE.py
@@ -0,0 +1,4 @@
+DB_LOGIN_INFO = {'credentials': 'mongodb://user:password@server',
+                 'port': 27017}
+
+DATABASE_NAME = 'cv_data'
